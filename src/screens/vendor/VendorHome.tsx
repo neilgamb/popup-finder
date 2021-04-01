2054efd0782da83361641edd623b293e2ef6c7f3
Neilson Gamble <neilson@scandy.co>
2021-04-01 10:56:43 -0500

    got getAddress to work. need to wire up to text input

--- src/screens/vendor/VendorHome.tsx
+++ src/screens/vendor/VendorHome.tsx
@@ -1,12 +1,8 @@
-import React, { createRef } from 'react'
-import {
-  SafeAreaView,
-  ScrollView,
-  View,
-  TextInput as Input,
-} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, ScrollView, View } from 'react-native'
 import { Headline, Title, useTheme } from 'react-native-paper'
 import { Formik } from 'formik'
+import MapView from 'react-native-maps'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_PLACES_API_KEY } from '@env'
 
@@ -21,9 +17,25 @@ import { useAuth } from '../../hooks/useAuth'
 const VendorHome = () => {
   const { presets, spacing, colors } = useTheme()
   const { userInfo } = useAuth()
-  const locationRef = createRef()
+
+  // temp
+  const [latitude, setLatitude] = useState(12.840575)
+  const [longitude, setLongitude] = useState(77.651787)
+  const [latitudeDelta, setLatitudeDelta] = useState(0.025)
+  const [longitudeDelta, setLongitudeDelta] = useState(0.025)
+
+  const getAddress = () => {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${latitude},${longitude}&key=${GOOGLE_PLACES_API_KEY}`
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => console.log(JSON.stringify(json)))
+  }
 
   //todo: follow this tut for fully custom auto complete — https://medium.com/debugger-off/how-to-use-google-autocomplete-api-s-and-react-native-maps-in-react-native-to-fetch-user-location-20d3f65af48b
+
+  useEffect(() => {
+    getAddress()
+  }, [])
 
   return (
     <DismissKeyboard>
@@ -32,8 +44,8 @@ const VendorHome = () => {
           initialValues={{
             name: '',
             location: '',
-            description: '',
-            foodType: '',
+            // description: '',
+            // foodType: '',
           }}
           onSubmit={(values) => console.log(values)}
         >
@@ -48,63 +60,37 @@ const VendorHome = () => {
                   Before you get started, tell us a little more about your pop
                   up...
                 </Title>
-                <View>
-                  <TextInput
-                    label='Pop Up Name'
-                    onChangeText={handleChange('name')}
-                    onBlur={handleBlur('name')}
-                    value={values.name}
-                  />
-                  <GooglePlacesAutocomplete
-                    // suppressDefaultStyles={true}
-                    query={{
-                      key: GOOGLE_PLACES_API_KEY,
-                      language: 'en', // language of the results
-                    }}
-                    onPress={(data, details) => console.log(data, details)}
-                    styles={{
-                      textInputContainer: {
-                        marginTop: spacing.sm,
-                      },
-                      poweredContainer: { height: 0 },
-                      powered: { height: 0 },
-                      listView: {
-                        paddingTop: spacing.xs,
-                        // paddingHorizontal: spacing.sm,
-                        backgroundColor: 'white',
-                        borderRadius: 8,
-                      },
-                      separator: { height: 0 },
-                      description: { color: colors.primary },
-                    }}
-                    textInputProps={{
-                      InputComp: Input,
-                      // leftIcon: { type: 'font-awesome', name: 'chevron-left' },
-                      // errorStyle: { color: 'red' },
+                <TextInput
+                  label='Pop Up Name'
+                  onChangeText={handleChange('name')}
+                  onBlur={handleBlur('name')}
+                  value={values.name}
+                />
+                <TextInput
+                  label='Location'
+                  onChangeText={handleChange('location')}
+                  onBlur={handleBlur('location')}
+                  value={values.location}
+                />
+                <View
+                  style={{
+                    flex: 1,
+                    backgroundColor: 'white',
+                    height: 200,
+                    marginTop: spacing.md,
+                    borderRadius: 16,
+                    overflow: 'hidden',
+                  }}
+                >
+                  <MapView
+                    style={{ flex: 1 }}
+                    initialRegion={{
+                      latitude,
+                      longitude,
+                      latitudeDelta,
+                      longitudeDelta,
                     }}
                   />
-                  {/* <TextInput
-                    label='Home City or Town'
-                    onChangeText={handleChange('location')}
-                    onBlur={handleBlur('location')}
-                    value={values.location}
-                  /> */}
-                  {/* <TextInput
-                    label='Description'
-                    placeholder='Briefly describe your pop up in a sentence or two'
-                    multiline
-                    onChangeText={handleChange('description')}
-                    onBlur={handleBlur('description')}
-                    value={values.description}
-                  />
-                  <View
-                    style={{
-                      width: '100%',
-                      height: 200,
-                      backgroundColor: 'white',
-                      marginTop: spacing.md,
-                    }}
-                  ></View> */}
                 </View>
               </ScrollView>
               <View style={presets.screenActions}>
