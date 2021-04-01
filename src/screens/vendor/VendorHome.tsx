2411b0aacd2b511405958abfd1df1c2e509443bd
Neilson Gamble <neilson@scandy.co>
2021-04-01 09:18:05 -0500

    adding google maps, places api. working on address autocomplete

--- src/screens/vendor/VendorHome.tsx
+++ src/screens/vendor/VendorHome.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import { SafeAreaView, ScrollView, View } from 'react-native'
+import React, { createRef } from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  TextInput as Input,
+} from 'react-native'
 import { Headline, Title, useTheme } from 'react-native-paper'
 import { Formik } from 'formik'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_PLACES_API_KEY } from '@env'
 
 import {
   ScreenHeader,
@@ -12,8 +19,11 @@ import {
 import { useAuth } from '../../hooks/useAuth'
 
 const VendorHome = () => {
-  const { presets, spacing } = useTheme()
+  const { presets, spacing, colors } = useTheme()
   const { userInfo } = useAuth()
+  const locationRef = createRef()
+
+  //todo: follow this tut for fully custom auto complete — https://medium.com/debugger-off/how-to-use-google-autocomplete-api-s-and-react-native-maps-in-react-native-to-fetch-user-location-20d3f65af48b
 
   return (
     <DismissKeyboard>
@@ -45,13 +55,41 @@ const VendorHome = () => {
                     onBlur={handleBlur('name')}
                     value={values.name}
                   />
-                  <TextInput
+                  <GooglePlacesAutocomplete
+                    // suppressDefaultStyles={true}
+                    query={{
+                      key: GOOGLE_PLACES_API_KEY,
+                      language: 'en', // language of the results
+                    }}
+                    onPress={(data, details) => console.log(data, details)}
+                    styles={{
+                      textInputContainer: {
+                        marginTop: spacing.sm,
+                      },
+                      poweredContainer: { height: 0 },
+                      powered: { height: 0 },
+                      listView: {
+                        paddingTop: spacing.xs,
+                        // paddingHorizontal: spacing.sm,
+                        backgroundColor: 'white',
+                        borderRadius: 8,
+                      },
+                      separator: { height: 0 },
+                      description: { color: colors.primary },
+                    }}
+                    textInputProps={{
+                      InputComp: Input,
+                      // leftIcon: { type: 'font-awesome', name: 'chevron-left' },
+                      // errorStyle: { color: 'red' },
+                    }}
+                  />
+                  {/* <TextInput
                     label='Home City or Town'
                     onChangeText={handleChange('location')}
                     onBlur={handleBlur('location')}
                     value={values.location}
-                  />
-                  <TextInput
+                  /> */}
+                  {/* <TextInput
                     label='Description'
                     placeholder='Briefly describe your pop up in a sentence or two'
                     multiline
@@ -59,6 +97,14 @@ const VendorHome = () => {
                     onBlur={handleBlur('description')}
                     value={values.description}
                   />
+                  <View
+                    style={{
+                      width: '100%',
+                      height: 200,
+                      backgroundColor: 'white',
+                      marginTop: spacing.md,
+                    }}
+                  ></View> */}
                 </View>
               </ScrollView>
               <View style={presets.screenActions}>
