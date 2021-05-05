60e19f5ee049ac8d7281e8a6a1259d2f33eff735
Neilson Gamble <neilson@scandy.co>
2021-05-05 14:21:11 -0500

    adding button prop to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,63 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react'
+import { Animated, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 import { theme } from '../style/theme'
 
 interface ProfileCardProps {
   children: React.ReactNode
+  button?: boolean
   style: any
 }
 
-const Card = ({ children, style }: ProfileCardProps) => (
-  <View style={{ ...styles.profileCardContainer, ...style }}>{children}</View>
-)
+const Card = ({ children, style, button }: ProfileCardProps) => {
+  const [isPressed, setIsPressed] = useState(false)
+  const pressedAnim = useRef(new Animated.Value(1)).current
+
+  useEffect(() => {
+    if (button) {
+      if (isPressed) {
+        Animated.timing(pressedAnim, {
+          toValue: 0,
+          duration: 25,
+          useNativeDriver: true,
+        }).start()
+      } else {
+        Animated.timing(pressedAnim, {
+          toValue: 1,
+          duration: 50,
+          useNativeDriver: true,
+        }).start()
+      }
+    }
+  }, [isPressed])
+
+  return (
+    <TouchableWithoutFeedback
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+    >
+      <Animated.View
+        style={{
+          ...styles.profileCardContainer,
+          ...style,
+          shadowOpacity: pressedAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0.01, 0.15],
+          }),
+          transform: [
+            {
+              scale: pressedAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0.995, 1],
+              }),
+            },
+          ],
+        }}
+      >
+        {children}
+      </Animated.View>
+    </TouchableWithoutFeedback>
+  )
+}
 
 export default Card
 
@@ -20,7 +68,6 @@ const styles = StyleSheet.create({
     marginHorizontal: theme.spacing.md,
     borderRadius: theme.roundness,
     marginTop: theme.spacing.md,
-    // padding: theme.spacing.md,
     position: 'relative',
     ...theme.boxShadow,
   },
