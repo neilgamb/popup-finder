0c63673fe70360a8f7174bfb016af527b586fa04
Neilson Gamble <neilson@scandy.co>
2021-05-14 14:49:58 -0500

    finishing basic eventcard elements

--- src/components/VendorEventCard.tsx
+++ src/components/VendorEventCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Dimensions, TouchableOpacity, View } from 'react-native'
-import { Title, Text, useTheme } from 'react-native-paper'
+import { Dimensions, StyleSheet, TouchableOpacity, View } from 'react-native'
+import { IconButton, Title, Text, useTheme } from 'react-native-paper'
 import { format } from 'date-fns'
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 import Carousel from 'react-native-snap-carousel'
@@ -8,6 +8,8 @@ import Carousel from 'react-native-snap-carousel'
 import { Card } from './index'
 import { Event } from '../hooks/index'
 import { mapStyle } from '../style/mapStyle'
+import { MENU_ITEM_CATEGORIES } from '../utils/constants'
+import { theme, withBorder } from '../style/theme'
 
 const screenWidth = Dimensions.get('window').width
 const sliderWidth = screenWidth
@@ -26,9 +28,6 @@ const VendorEventCard = ({ event }: Props) => {
 
   const carouselRef = useRef<Carousel<Item>>(null)
 
-  const lat = Number(event?.locationData?.geometry?.location?.lat)
-  const lng = Number(event?.locationData?.geometry?.location?.lng)
-
   const renderItems = ({ item }) => <View style={{ flex: 1 }}>{item}</View>
 
   useEffect(() => {
@@ -37,25 +36,31 @@ const VendorEventCard = ({ event }: Props) => {
 
   return (
     <Card style={{ marginTop: spacing.md }}>
+      <IconButton
+        icon='pencil'
+        style={styles.editButton}
+        color={colors.gray}
+        size={25}
+        onPress={() => console.log('Pressed')}
+      />
       <Carousel
         layout='default'
         activeSlideAlignment='center'
         ref={carouselRef}
         inactiveSlideScale={1}
-        data={[
-          <EventInfo event={event} />,
-          <View style={{ flex: 1, height: 200, backgroundColor: 'blue' }} />,
-        ]}
+        data={[<EventInfo event={event} />, <EventMenu event={event} />]}
         swipeThreshold={1}
         sliderWidth={sliderWidth - spacing.md * 2}
         itemWidth={sliderWidth - spacing.md * 2}
         enableMomentum
+        scrollEnabled={false}
         lockScrollWhileSnapping
         renderItem={renderItems}
         onSnapToItem={setActiveIndex}
       />
       <View style={{ flexDirection: 'row' }}>
         <TouchableOpacity
+          disabled={activeIndex === 0}
           onPress={() => setActiveIndex(0)}
           style={{
             flex: 1,
@@ -66,9 +71,10 @@ const VendorEventCard = ({ event }: Props) => {
               activeIndex === 0 ? colors.extraLightGray : 'white',
           }}
         >
-          <Text>DETAILS</Text>
+          <Text style={{ color: colors.gray }}>DETAILS</Text>
         </TouchableOpacity>
         <TouchableOpacity
+          disabled={activeIndex === 1}
           onPress={() => setActiveIndex(1)}
           style={{
             flex: 1,
@@ -79,7 +85,7 @@ const VendorEventCard = ({ event }: Props) => {
               activeIndex === 1 ? colors.extraLightGray : 'white',
           }}
         >
-          <Text>MENU</Text>
+          <Text style={{ color: colors.gray }}>MENU</Text>
         </TouchableOpacity>
       </View>
     </Card>
@@ -135,3 +141,55 @@ const EventInfo = ({ event }: Props) => {
     </>
   )
 }
+
+const EventMenu = ({ event }: Props) => {
+  const { spacing, colors } = useTheme()
+
+  return (
+    <View
+      style={{
+        padding: spacing.sm,
+        width: '90%',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        maxHeight: 200,
+        overflow: 'hidden',
+      }}
+    >
+      {MENU_ITEM_CATEGORIES.map((cat, catIndex) => {
+        return (
+          event.menu.some((menuItem) => menuItem.category === cat.value) && (
+            <View
+              key={catIndex}
+              style={{
+                marginBottom: spacing.sm,
+                marginRight: spacing.sm,
+              }}
+            >
+              <Text style={{ fontSize: 18 }}>{cat.label}</Text>
+              {event.menu
+                .filter((menuItem) => menuItem.category === cat.value)
+                .map((menuItem, menuItemIndex) => (
+                  <Text
+                    key={menuItemIndex}
+                    style={{ fontSize: 16, color: colors.gray }}
+                  >
+                    {menuItem.name}
+                  </Text>
+                ))}
+            </View>
+          )
+        )
+      })}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  editButton: {
+    position: 'absolute',
+    right: theme.spacing.xxs,
+    top: theme.spacing.xxs,
+    zIndex: 1,
+  },
+})
